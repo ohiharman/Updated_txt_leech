8c2a20b12524f25959e2084f7ce60ab3ece9ccf7
Aarohi9532 <ramprahary22@gmail.com>
2025-06-15 23:21:11 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,7 +5,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "23916035"))
 API_HASH = environ.get("API_HASH", "b1d6e8c276e940e1aff7278749b0b4f6")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7156010503:AAFRff6RZpuMta78wjzhUT-xB_Eu0NpTqEo")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 OWNER = int(environ.get("OWNER", "1263635239"))
 CREDIT = "𝄟⃝‌🐬🇳‌ɪᴋʜɪʟ𝄟⃝🐬"
 AUTH_USER = os.environ.get('AUTH_USERS', '1263635239').split(',')
